5df3511aafc80be168633b0fd1666c4d8b3ad447
Norma Timoteo Mendes da Silva <102174411+normatm@users.noreply.github.com>
2022-04-24 21:52:46 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ def calcular_area_do_quadrado(lado):
 
 
 def calcular_area_do_triangulo(lado, comprimento):
-    return lado * comprimento/2
+    return lado * comprimento / 2
+
 
 def contagem_progressiva(fim):
     for numero in range(fim):  # repetir o bloco de zero até o fim
@@ -31,8 +32,45 @@ def apoiar_candidato(nome, vezes):
             print(numero+1, '-',nome)
 
 
-# Receber função para escrever
+#def brincar_de_plim(fim):
+	#for numero in range(fim):      # brincadeira igual do Silvio Santos
+		#if  numero % 4 == 0:      # % não calcula percentual e sim calcula o que sobra
+            #print('PLIM!')
+        #else:
+            #print('{:0>3}'.format(numero))
 
+def exibir_dia_da_semana_if(numero):
+    if numero == 1:
+        print('O dia é segunda')
+    elif numero == 2:
+        print('O dia é terça')
+    elif numero == 3:
+        print('O dia é quarta')
+    elif numero == 4:
+        print('O dia é quinta')
+    elif numero == 5:
+        print('O dia é sexta')
+    elif numero == 6:
+        print('O dia é sabado')
+    elif numero == 7:
+        print('O dia é domingo')
+    else:
+        print("Numero de dia invalido, Digite um numero de 1 a 7")
+
+
+def brincar_de_para_ou_continua():
+    resposta = 'C'           # True (verdadeiro) neste caso - C que Continua
+
+
+    #while resposta == 'C' or resposta == 'c':  # == True
+    while resposta.upper() == 'C':            #("Digite C para Continuar ou qualquer outro caracter para Parar")
+        resposta = input("Digite C para Continuar ou qualquer outro caracter para Parar")
+
+    print('Voce decidiu Parar com a brincadeira')
+
+
+
+# Receber função para escrever
 # Chamar função
 # Ex. print_hi (Norma)
 # Para salvar  Ctrl + S
@@ -71,11 +109,11 @@ if __name__ == '__main__':
     # RESULTADO =  A área do quadrado é de 25 m²
 
     # Chamar função Area do Triangulo
-    resultado = calcular_area_do_triangulo(6,7)
+    resultado = calcular_area_do_triangulo(6, 7)
     print(f'A area do triangulo e de {resultado} m²')
 
     # RESULTADO =  A área do triangulo é de 21.0 m²
-    
+
     #  Chamar / Executar uma Contagem Progressiva
     resultado = contagem_progressiva(11)
 
@@ -86,3 +124,24 @@ if __name__ == '__main__':
     # Chamar / Executar Função =
 	#brincar_de_plim(10)
     #RESULTADO=Ele executou PLIM! Por 10 vezes e escreveu Plim múltiplos de 4
+
+    # exemplo de dia da semana com if - elif - else
+    exibir_dia_da_semana_if(1)
+    exibir_dia_da_semana_if(2)
+    exibir_dia_da_semana_if(3)
+    exibir_dia_da_semana_if(4)
+    exibir_dia_da_semana_if(5)
+    exibir_dia_da_semana_if(6)
+    exibir_dia_da_semana_if(7)
+    exibir_dia_da_semana_if(' ')
+    # RESULTADO=Ele executou os dias da semana de 1 a 7 =(seg ter qua qui sex sab dom)
+    # Numero de dia invalido, Digite um numero de 1 a 7
+
+    # exemplo de dia da semana com Match - case
+    #exibir_dia_da_semana_match(1)
+    # RESULTADO=Ele não executou informa que a versão deve ser 3.10
+
+    # exemplo com while - brincar de x  para ou continua
+    brincar_de_para_ou_continua()
+    # RESULTADO=Ele não executou informa que a versão deve ser 3.10
+    #Digite C para Continuar ou qualquer outro caracter para Parar
